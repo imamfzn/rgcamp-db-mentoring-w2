7072af3901f1e2248e06bef447695ebe4074b09c
imam.fauzan <imam.fauzan@bukalapak.com>
2022-11-25 14:00:10 +0700

    simplify

--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"mentoring/db"
 	"mentoring/model"
-	"mentoring/repository"
 )
-
-func printEmployees(emps []model.Employee) {
-	for _, emp := range emps {
-		fmt.Println(emp.ID, emp.Name)
-	}
-}
 
 func main() {
 	credential := db.Credential{
-		Host:         "",
+		Host:         "db.fznlabs.xyz",
 		Username:     "",
 		Password:     "",
 		DatabaseName: "",
@@ -32,46 +23,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	repo := repository.NewRepo(db)
-
-	start := 3000000
-	end := 5000000
-	names, err := repo.GetAllEmployeeNameBySalaryRange(start, end)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Employee dengan range gaji %d - %d: %+v\n", start, end, names)
-
-	departmentName := "Research & Development"
-	emps, err := repo.GetAllEmployeeByDepartmentName(departmentName)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Employee di Department %s:\n", departmentName)
-	printEmployees(emps)
-
-	roomId := 3
-	emps, err = repo.GetAllEmployeeByRoomId(roomId)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Employee yang kerja di ruangan: %d:\n", roomId)
-	printEmployees(emps)
-
-	roomLevel := 10
-	avg, err := repo.GetAverageSalaryByRoomLevel(roomLevel)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Rata-rata salary di lantai %d: %f\n", roomLevel, avg)
-
-	rooms, err := repo.GetEmptyRooms()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Ruangan tanpa penghuni:", rooms)
 }
